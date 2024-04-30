69f349d473e39f2a3291403a27352edc9689458a
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-30 18:24:44 +0200

    Initial commit

---
+++ src/test/java/com/concerttracker/concerttracker/ConcertTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concerttracker.concerttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
